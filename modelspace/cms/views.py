7b5d7276c135b9f49c942d10c056afc7b835555c
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2019-07-17 00:22:51 +0100

    Add pagination

--- modelspace/cms/views.py
+++ modelspace/cms/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.decorators.http import require_POST
 
@@ -6,7 +7,10 @@ from .forms import UploadForm
 
 def index(request):
     template = 'cms/index.html'
-    uploads = STLFile.objects.all()
+    uploads_list = STLFile.objects.all()
+    paginator = Paginator(uploads_list, 6) # 6 uploads per page
+    page = request.GET.get('page')
+    uploads = paginator.get_page(page)
     context = {'uploads': uploads}
     return render(request, template, context)
 
