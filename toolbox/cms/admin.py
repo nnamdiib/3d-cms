b7c2d511d5e510cf56f10f7bf6f9c2d3d11904fa
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2019-07-16 01:26:05 +0100

    Add STLFile to django admin site.

--- toolbox/cms/admin.py
+++ toolbox/cms/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import STLFile
 
-
+@admin.register(STLFile)
 class STLFileAdmin(admin.ModelAdmin):
 	list_display = ('name', 'date_created', 'date_updated')
 
