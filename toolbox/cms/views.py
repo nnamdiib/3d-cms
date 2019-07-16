6f7b593c2655763a0612cb863296ad1eb5bb8640
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2019-07-16 01:25:32 +0100

    Add views and corresponding templates and URLs

--- toolbox/cms/views.py
+++ toolbox/cms/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
 
 from .models import STLFile
-
+from .forms import UploadForm
 
 def index(request):
-	context = {}
-	template = 'index.html'
-	
+    context = {}
+    template = 'cms/index.html'
+    
+    return render(request, template, context)
 
-	return render(request, template, context)
+
+def upload(request):
+    template = 'cms/upload.html'
+    form = UploadForm(request.POST or None, request.FILES or None)
+    print(request.POST)
+    if form.is_valid():
+        print('VALID *******')
+        print(form.cleaned_data)
+        form.save()
+        return index(request)
+
+    context = {'form': form}
+    return render(request, template, context)
