4e34b79335c9867d6a3d7929d23729ba0f9f0a1f
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2019-07-16 22:52:10 +0100

    Add tagging to settings and model

--- modelspace/cms/models.py
+++ modelspace/cms/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from tagulous.models import TagField
 
 class STLFile(models.Model):
     name = models.CharField(max_length=225)
     document = models.FileField(upload_to='uploads/')
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-
+    tags = TagField()
+    
     def __str__(self):
         return self.name
