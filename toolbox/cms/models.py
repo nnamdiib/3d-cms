e89a00e1d4114f5e8882ece446f555543f13eabc
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2019-07-16 01:32:56 +0100

    Remove path field from STLFile model

--- toolbox/cms/models.py
+++ toolbox/cms/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 
 class STLFile(models.Model):
-	name = models.CharField(max_length=225)
-	path = models.URLField()
-	date_created = models.DateTimeField()
-	date_updated = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=225)
+    document = models.FileField(upload_to='uploads/')
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
+
