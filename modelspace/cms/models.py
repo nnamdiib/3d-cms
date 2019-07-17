f38f74ada3a5bdc03adecd51cc012094e76765ba
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2019-07-17 11:20:18 +0300

    tags field optional

--- modelspace/cms/models.py
+++ modelspace/cms/models.py
@@ -6,7 +6,7 @@ class STLFile(models.Model):
     document = models.FileField(upload_to='uploads/')
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    tags = TagField()
+    tags = TagField(blank=True)
     
     def __str__(self):
         return self.name
