9c395572c30e4e9a2be7223a0e12b70d1bb503bc
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2019-07-19 22:55:10 +0100

    Bug fixes

--- cms/urls.py
+++ cms/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('upload/', views.upload, name='upload'),
     path('download/<int:file_id>/', views.download, name='download'),
-    path('detail/<int:stl_id>/', views.detail, name='detail')
+    path('detail/<int:stl_id>/', views.detail, name='detail'),
     path('delete/<int:file_id>/', views.delete, name='delete'),
 ]
