1a13bba422e31c47781f595bb8f6eb897a2591b9
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2019-07-20 12:50:38 +0300

    blank=True

--- cms/models.py
+++ cms/models.py
@@ -7,7 +7,7 @@ class STLFile(models.Model):
     document = models.FileField(upload_to='uploads/')
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    tags = TaggableManager()
+    tags = TaggableManager(blank=True)
     
     def __str__(self):
         return self.name
