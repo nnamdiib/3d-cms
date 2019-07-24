a4e6ab0957ab5782cb6ce7ee3e1f3d1315acc764
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2019-07-24 14:44:52 +0100

    Add slight refactors

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_POST
@@ -9,25 +11,38 @@ from .models import STLFile
 from .forms import UploadForm, UpdateForm
 from .utils import create_thumbnail
 
-import os
 
-per_page = 8
+PER_PAGE = 8
+
+def make_paginated_entries(entries, request):
+    """
+    Helper function used to make a paginated list of model entries
+    Used in views.index and views.search
+    """
+    paginator = Paginator(entries, PER_PAGE)
+    page = request.GET.get('p')
+    request.session['page'] = page
+    return paginator.get_page(page)
 
 def index(request):
+    template = 'cms/index.html'
     q = request.GET.get('q', None)
-
     if q:
         return search(request, q)
         
-    template = 'cms/index.html'
     entries = STLFile.objects.all().order_by('-date_created')
 
-    paginator = Paginator(entries, per_page)
-    page = request.GET.get('p')
-    request.session['page'] = page
-    uploads = paginator.get_page(page)
-
+    uploads = make_paginated_entries(entries, request)
     context = {'uploads': uploads}
+    return render(request, template, context)
+
+def search(request, q):
+    template = 'cms/index.html'
+    entries = STLFile.objects.filter(
+        Q(name__icontains=q) | Q(tags__name__icontains=q)).distinct()
+
+    paginated_entries = make_paginated_entries(entries)
+    context = {'uploads': paginated_entries, 'count': entries.count(), 'q':q}
     return render(request, template, context)
 
 def upload(request):
@@ -47,63 +62,6 @@ def upload(request):
     context = {'form': form}
     return render(request, template, context)
 
-def search(request, q):
-    template = 'cms/index.html'
-    entries = STLFile.objects.filter(Q(name__icontains=q) | Q(tags__name__icontains=q)).distinct()
-
-    paginator = Paginator(entries, per_page)
-    page = request.GET.get('p')
-    request.session['page'] = page
-    uploads = paginator.get_page(page)
-
-    context = {'uploads': uploads, 'count': entries.count(), 'q':q}
-    return render(request, template, context)
-
-def save(request, file_id):
-    stl = STLFile.objects.get(pk=file_id)
-    file_path = stl.document.path
-    if os.path.exists(file_path):
-        with open(file_path, 'rb') as fh:
-            response = HttpResponse(fh.read(), content_type="model/stl")
-            extension = os.path.splitext(file_path)[-1]
-            response['Content-Disposition'] = 'inline; filename=' + stl.name + extension
-            return response
-    raise Http404
-
-def get_file(request, file_name):
-    name, extension = file_name.split('.')[0], file_name.split('.')[1]
-    stl = get_object_or_404(STLFile, file_name=name)
-    file_path = stl.document.path
-    if os.path.exists(file_path):
-        with open(file_path, 'rb') as fh:
-            response = HttpResponse(fh.read(), content_type="model/stl")
-            response['Content-Disposition'] = 'inline; filename=' + name + extension
-            return response
-    raise Http404            
-
-def detail(request, stl_id):
-    template = 'cms/detail.html'
-    stl = get_object_or_404(STLFile, pk=stl_id)
-    file_path = '\\\\'.join(stl.document.path.split('\\'))
-    context = {'upload': upload, 'file_path': file_path, 'upload': stl}
-    return render(request, template, context)
-
-def erase(request, file_id):
-    stl = get_object_or_404(STLFile, pk=file_id)
-    stl_file = os.path.join(settings.BASE_DIR, stl.document.path)
-    png_path = os.path.join(settings.THUMBS_ROOT, stl.file_name + '.png')
-    try:
-        os.remove(stl_file)
-        os.remove(png_path)
-    except FileNotFoundError:
-        print("Error while attempting to delete file(s).")
-    stl.tags.clear()
-    stl.delete()
-    page = request.session['page']
-    if page:
-        return redirect("/" + "?p=" + str(page))
-    return redirect("/")
-        
 def edit(request, file_id):
     template = 'cms/upload.html'
     stl = get_object_or_404(STLFile, pk=file_id)
@@ -141,3 +99,53 @@ def edit(request, file_id):
     update_form = UpdateForm(initial=initial_data)
     context = {'form': update_form}
     return render(request, template, context)
+
+def save(request, file_id):
+    stl = STLFile.objects.get(pk=file_id)
+    file_path = stl.document.path
+    if os.path.exists(file_path):
+        return get_download(file_path)
+    raise Http404
+
+def fetch(request, file_name):
+    name, extension = file_name.split('.')[0], file_name.split('.')[1]
+    stl = get_object_or_404(STLFile, file_name=name)
+    file_path = stl.document.path
+    if os.path.exists(file_path):
+        return get_download(file_path)
+    raise Http404
+
+def get_download(file_path):
+    """
+    Helper function used to prepare a .stl file and send it
+    for download in the browser client.
+    Used in views.save and views.fetch
+    """
+    with open(file_path, 'rb') as fh:
+        response = HttpResponse(fh.read(), content_type="model/stl")
+        extension = os.path.splitext(file_path)[-1]
+        response['Content-Disposition'] = 'inline; filename=' + stl.name + extension
+        return response
+
+def detail(request, stl_id):
+    template = 'cms/detail.html'
+    stl = get_object_or_404(STLFile, pk=stl_id)
+    context = {'upload': upload, 'upload': stl}
+    return render(request, template, context)
+
+def erase(request, file_id):
+    stl = get_object_or_404(STLFile, pk=file_id)
+    stl_file = os.path.join(settings.BASE_DIR, stl.document.path)
+    png_path = os.path.join(settings.THUMBS_ROOT, stl.file_name + '.png')
+    try:
+        os.remove(stl_file)
+        os.remove(png_path)
+    except FileNotFoundError:
+        print("Error while attempting to delete file(s).")
+    stl.tags.clear()
+    stl.delete()
+    page = request.session['page']
+    if page:
+        return redirect("/" + "?p=" + str(page))
+    return redirect("/")
+        
