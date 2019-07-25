cfa9543fadc794cc0f90b3799e3f96446ec69a39
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2019-07-26 00:23:50 +0300

    switch to pyrender/linux only thumbnails

--- cms/utils.py
+++ cms/utils.py
@@ -1,6 +1,11 @@
 import subprocess
 import os
 import platform
+os.environ["PYOPENGL_PLATFORM"] = "egl"
+import numpy as np
+import trimesh
+import pyrender
+import matplotlib.pyplot as plt
 # This file contains various helper functions needed for the app.
 # These helpers do not fit perfectly into the django structure of models,
 # views and controllers so we have created a special place for them.
@@ -13,14 +18,37 @@ import platform
 # 	 is a requirement for this function
 
 def create_thumbnail(stl_path, output_path):
-	stl_thumb_exe = 'C:\\Program Files\\stl-thumb\\stl-thumb.exe'  # Default is windows
-	if platform.system() == 'Linux':
-		# We are on a linux machine
-		stl_thumb_exe = 'stl-thumb'
+	model_data = trimesh.load(stl_path)
+	mesh = pyrender.Mesh.from_trimesh(model_data)
+	scene = pyrender.Scene()
+	scene.add(mesh)
 
-	size = '200'  # Specify a 200 x 200 pixel PNG
-	command = [stl_thumb_exe, stl_path, output_path, '-s', size]
-	print(command)
-	process = subprocess.run(command)
-	if process.returncode == 0:
-		print('Created Thumbnail at {}'.format(output_path))
+	# Set up the camera -- z-axis away from the scene, x-axis right, y-axis up
+	camera = pyrender.PerspectiveCamera(yfov=np.pi / 3.0)
+	s = np.sqrt(2)/2
+
+	camera_pose = np.array([
+       [0.0, -s,   s,   0.3],
+       [1.0,  0.0, 0.0, 0.0],
+       [0.0,  s,   s,   0.35],
+       [0.0,  0.0, 0.0, 1.0],
+    ])
+
+	scene.add(camera, pose=camera_pose)
+
+	# Set up the light -- a single spot light in the same spot as the camera
+	light = pyrender.SpotLight(color=np.ones(3), intensity=3.0,
+                               innerConeAngle=np.pi/16.0)
+	
+	scene.add(light, pose=camera_pose)
+
+	# Render the scene
+	r = pyrender.OffscreenRenderer(200, 200)
+	color, depth = r.render(scene)
+
+	# Show the images
+	plt.figure()
+	plt.subplot(1,1,1)
+	plt.axis('off')
+	plt.imshow(color)
+	plt.savefig(output_path)
