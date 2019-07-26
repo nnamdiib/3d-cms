600017832a14a4c87cf0f58457c60ac766108234
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2019-07-26 22:54:09 +0100

    Modify views and templates to use new MainFile model

--- cms/models.py
+++ cms/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from taggit.managers import TaggableManager
 
+from .utils import extract_file_name
+
+
 class Entry(models.Model):
     name = models.CharField(max_length=225)
-    main_file = models.FileField(upload_to='uploads/')
-    file_name = models.CharField(max_length=255, blank=True, null=True)
-    # extra_files = models.FileField(upload_to='uploads', null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     tags = TaggableManager(blank=True)
@@ -13,18 +13,13 @@ class Entry(models.Model):
     def __str__(self):
         return self.name
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs) # Call the real save method
-        self.file_name = self.get_name_with_extension()
-        super().save(*args, **kwargs) # Call the real save method
-
     def get_name_without_extension(self):
         return self.main_file.name.split('/')[1].split('.')[0]
 
     def get_name_with_extension(self):
         return self.main_file.name.split('/')[1]
 
-
+# To Do, create  GenericFile superclass for MainFile and EntryFile
 class MainFile(models.Model):
     entry = models.OneToOneField(Entry, on_delete=models.CASCADE)
     document = models.FileField(upload_to='uploads/')
@@ -33,6 +28,14 @@ class MainFile(models.Model):
 
     def __str__(self):
         return self.entry.name
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs) # Call the real save method
+        self.file_name = extract_file_name(self.document.path)
+        super().save(*args, **kwargs) # Call the real save method
+
+    def remove_extension(self):
+        return self.file_name.split('.')[0]
 
 
 class ExtraFile(models.Model):
--- cms/views.py
+++ cms/views.py
@@ -8,9 +8,9 @@ from django.conf import settings
 from django.http import HttpResponse, Http404
 from django.urls import reverse
 
-from .models import Entry, ExtraFile
+from .models import Entry, MainFile, ExtraFile
 from .forms import UploadForm
-from .utils import create_thumbnail
+from .utils import create_thumbnail, extract_file_name
 
 PER_PAGE = 8
 
@@ -29,8 +29,8 @@ def index(request):
     q = request.GET.get('q', None)
     if q:
         return search(request, q)
-        
-    entries = Entry.objects.all().order_by('-date_created')
+    
+    entries = MainFile.objects.all().select_related('entry').order_by('-date_created')
 
     uploads = make_paginated_entries(entries, request)
     context = {'uploads': uploads}
@@ -53,27 +53,22 @@ def upload(request):
         main_doc = request.FILES['main_file']
         tags = form.cleaned_data['tags']
         extra_files = request.FILES.getlist('extra_files')
-        entry = Entry.objects.create(
-            name=form.cleaned_data['name'],
-            main_file=main_doc,
-        )
+
+        entry = Entry.objects.create(name=form.cleaned_data['name'])
+        main_file = MainFile.objects.create(entry=entry, document=main_doc)
 
         if tags:
             [entry.tags.add(tag.strip()) for tag in tags.split(',')]
-        entry.file_name = entry.get_name_with_extension()
         entry.save()
 
         for file in extra_files:
-            ef = ExtraFile.objects.create(
-                entry=entry,
-                document=file
-            )
+            ef = ExtraFile.objects.create(entry=entry, document=file)
             ef.file_name = ef.get_name_with_extension()
             ef.save()
 
-        name = entry.get_name_without_extension()
+        name = main_file.file_name.split('.')[0]
         output_path = os.path.join(settings.THUMBS_ROOT, name + '.png')
-        create_thumbnail(entry.main_file.path, output_path)
+        create_thumbnail(main_file.document.path, output_path)
         return index(request)
 
     context = {'form': form}
@@ -82,8 +77,8 @@ def upload(request):
 def edit(request, entry_id):
     template = 'cms/upload.html'
     entry = get_object_or_404(Entry, pk=entry_id)
-    png_path = os.path.join(settings.THUMBS_ROOT, entry.file_name + '.png')
-
+    main_file = MainFile.objects.get(entry=entry)
+    
     update_form = UploadForm(request.POST or None, request.FILES or None)
     update_form.fields['main_file'].required = False
     update_form.fields['extra_files'].required = False
@@ -91,24 +86,25 @@ def edit(request, entry_id):
     if update_form.is_valid():
         extra_files = request.FILES.getlist('extra_files')
         entry.name = update_form.cleaned_data['name']
-
+        entry.save()
         # Handle a new main file
         if update_form.cleaned_data['main_file']:
+            old_doc_path = main_file.document.path
+            name =  main_file.file_name.split('.')[0]
+            old_png_path = os.path.join(settings.THUMBS_ROOT, name + '.png')
+            
+            main_file.document = update_form.cleaned_data['main_file']
+            main_file.save()
+
             try:
-                os.remove(entry.main_file.path)
-                os.remove(png_path)
+                os.remove(old_doc_path)
+                os.remove(old_png_path)
             except FileNotFoundError:
                 print('Error while attempting to delete file(s).')
 
-            entry.main_file = update_form.cleaned_data['main_file']
-            entry.save()  # Necessary to obtain a unique document name
-
-            entry.file_name = entry.get_name_with_extension()
-            entry.save()
-
-            name = entry.get_name_without_extension()
+            name = main_file.file_name.split('.')[0]
             png_path = os.path.join(settings.THUMBS_ROOT, name + '.png')
-            create_thumbnail(entry.main_file.path, png_path)
+            create_thumbnail(main_file.document.path, png_path)
 
         # Handle (any) new tags
         entry.tags.clear()
@@ -124,7 +120,7 @@ def edit(request, entry_id):
                     entry=entry,
                     document=file
                 )
-                ef.file_name = ef.get_name_with_extension()
+                ef.file_name = extract_file_name(ef.document.path)
                 ef.save()
 
         return redirect('index')
@@ -132,7 +128,7 @@ def edit(request, entry_id):
     initial_data = {
         'name': entry.name,
         'tags': ', '.join([t.name for t in entry.tags.all()]),
-        'main_file': entry.main_file,
+        'main_file': main_file.document,
     }
 
     update_form = UploadForm(initial=initial_data)
@@ -148,9 +144,10 @@ def edit(request, entry_id):
 
 def save(request, file_id):
     entry = Entry.objects.get(pk=file_id)
-    file_path = entry.main_file.path
+    mf = MainFile.objects.get(entry=entry)
+    file_path = main_file.document.path
     if os.path.exists(file_path):
-        return get_download(file_path, entry.file_name)
+        return get_download(file_path, main_file.file_name)
     raise Http404
 
 def fetch(request, file_name, file_type=None):
@@ -158,8 +155,8 @@ def fetch(request, file_name, file_type=None):
         ef = get_object_or_404(ExtraFile, file_name=file_name)
         file_path = ef.document.path
     else:
-        entry = get_object_or_404(Entry, file_name=file_name)
-        file_path = entry.main_file.path
+        main_file = get_object_or_404(MainFile, file_name=file_name)
+        file_path = main_file.document.path
     if os.path.exists(file_path):
         return get_download(file_path, file_name)
     raise Http404
@@ -179,17 +176,20 @@ def get_download(file_path, name):
 def detail(request, stl_id):
     template = 'cms/detail.html'
     entry = get_object_or_404(Entry, pk=stl_id)
+    main_file = get_object_or_404(MainFile, entry=entry)
     context = {
-        'upload': upload,
-        'upload': entry,
-        'extra_files': entry.extras.all()
+        'entry': entry,
+        'main_file': main_file,
+        'extra_files': entry.extras.all(),
+        'detail': True
     }
     return render(request, template, context)
 
 def erase(request, file_id):
     stl = get_object_or_404(Entry, pk=file_id)
-    thumb_name = str(stl.get_name_without_extension()) + '.png'
-    stl_file = os.path.join(settings.BASE_DIR, stl.main_file.path)
+    main_file = get_object_or_404(MainFile, entry=stl)
+    thumb_name = main_file.file_name.split('.')[0] + '.png'
+    stl_file = os.path.join(settings.BASE_DIR, main_file.document.path)
     png_path = os.path.join(settings.THUMBS_ROOT, thumb_name)
     try:
         os.remove(stl_file)
