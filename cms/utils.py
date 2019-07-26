38fb45c1df7a2ec1494527120d1d12b45ce92d51
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2019-07-26 03:40:21 +0300

    revert back thumb

--- cms/utils.py
+++ cms/utils.py
@@ -12,7 +12,7 @@ import platform
 # 	 to offer a preview of the STL file. Install https://github.com/unlimitedbacon/stl-thumb/
 # 	 is a requirement for this function
 
-def create_thumbnail_windows(stl_path, output_path):
+def create_thumbnail(stl_path, output_path):
 	stl_thumb_exe = 'C:\\Program Files\\stl-thumb\\stl-thumb.exe'  # Default is windows
 	if platform.system() == 'Linux':
 		# We are on a linux machine
@@ -24,48 +24,3 @@ def create_thumbnail_windows(stl_path, output_path):
 	process = subprocess.run(command)
 	if process.returncode == 0:
 		print('Created Thumbnail at {}'.format(output_path))
-
-def create_thumbnail_linux(stl_path, output_path):
-	os.environ["PYOPENGL_PLATFORM"] = "egl"
-	import numpy as np
-	import trimesh
-	import pyrender
-	import matplotlib.pyplot as plt
-
-	model_data = trimesh.load(stl_path)
-	mesh = pyrender.Mesh.from_trimesh(model_data)
-	scene = pyrender.Scene()
-	scene.add(mesh)
-
-	# Set up the camera -- z-axis away from the scene, x-axis right, y-axis up
-	camera = pyrender.PerspectiveCamera(yfov=np.pi / 3.0)
-	s = np.sqrt(2)/2
-
-	camera_pose = np.array([
-       [0.0, -s,   s,   0.3],
-       [1.0,  0.0, 0.0, 0.0],
-       [0.0,  s,   s,   0.35],
-       [0.0,  0.0, 0.0, 1.0],
-    ])
-
-	scene.add(camera, pose=camera_pose)
-
-	# Set up the light -- a single spot light in the same spot as the camera
-	light = pyrender.SpotLight(color=np.ones(3), intensity=3.0,
-                               innerConeAngle=np.pi/16.0)
-	
-	scene.add(light, pose=camera_pose)
-
-	# Render the scene
-	r = pyrender.OffscreenRenderer(200, 200)
-	color, depth = r.render(scene)
-
-	# Show the images
-	plt.figure()
-	plt.subplot(1,1,1)
-	plt.axis('off')
-	plt.imshow(color)
-	plt.savefig(output_path)
-
-
-create_thumbnail = create_thumbnail_linux if platform.system() == 'Linux' else create_thumbnail_windows
