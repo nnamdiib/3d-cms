0495c05d6629a7de44ee5bf03ffe035d94bbfc03
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2019-07-28 02:42:40 +0300

    one liner

--- cms/models.py
+++ cms/models.py
@@ -26,8 +26,7 @@ class Entry(models.Model):
         new_file.save()
 
     def update_entry(self, name=None, tags=None, main_file=None, extra_files=None):
-        if name:
-            self.name = name
+        self.name = name or self.name
         if tags:
             self.tags.clear()
             for tag in tags.split(','):
