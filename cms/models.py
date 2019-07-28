b3afa334e15a030eba8494785350d2858460a884
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2019-07-28 23:20:01 +0300

    better

--- cms/models.py
+++ cms/models.py
@@ -17,20 +17,24 @@ class Entry(models.Model):
                 object.delete()        
         super().delete(*args, **kwargs)
 
+    def add_file(self, object, file):
+        object.objects.create(entry=self, document=file).save()
+
     def update_entry(self, name=None, tags=None, main_file=None, extra_files=None):
         self.name = name or self.name
         if tags:
             self.tags.clear()
-            for tag in tags.split(','):
-                self.tags.add(tag.strip()) 
+            [self.tags.add(tag.strip()) for tag in tags.split(',')]
         if main_file:
-            updated, created = MainFile.objects.update_or_create(entry=self, defaults={"document": main_file})
-            create_thumbnail(MainFile.objects.get(entry=self).document.path)
+            if MainFile.objects.filter(entry=self).exists():
+                MainFile.objects.get(entry=self).delete()
+            self.add_file(MainFile, main_file)
         if extra_files:
             for file in extra_files:
-                ExtraFile.objects.create(entry=self, document=file)
+                self.add_file(ExtraFile, file)
 
 class File(models.Model):
+    entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
     document = models.FileField(upload_to='uploads/')
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
@@ -44,6 +48,10 @@ class File(models.Model):
 
 class MainFile(File):
     entry = models.OneToOneField(Entry, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        create_thumbnail(self.document.path)
 
     def delete(self, *args, **kwargs):
         delete_thumbnail(self.document.path)
