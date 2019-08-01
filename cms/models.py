65f5fc3039dc466f4877ef92f766acd70a730d45
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2019-08-01 19:40:12 +0100

    Add user field to each entry

--- cms/models.py
+++ cms/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 from taggit.managers import TaggableManager
 
 from .utils import *
 
 class Entry(models.Model):
+    user = models.ForeignKey(User, related_name='entry', on_delete=models.CASCADE, default=1)
     name = models.CharField(max_length=140)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
